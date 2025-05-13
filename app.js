2b9c67546a1402adc4b0feff23383a175daad8c2
iago2345-hi <iago.pereira23@etepd.com>
2025-05-13 15:27:51 -0300

    Create app.js
    não está completo

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import LoginScreen from '/workspaces/Simple-origami/MeuAppLogin/assets/images/dragão2-removebg-preview 1.png';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <LoginScreen />
+    </SafeAreaView>
+  );
+}
