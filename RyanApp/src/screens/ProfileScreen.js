08de28b68524029389ccb1311533e4e3cd1c30e8
iago2345-hi <iago.pereira23@etepd.com>
2025-05-18 20:58:07 -0300

    ProfileScreen.js

---
+++ RyanApp/src/screens/ProfileScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Perfil</Text>
+      <Button title="Sair" onPress={() => navigation.navigate('Login')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 32, marginBottom: 20 },
+});
