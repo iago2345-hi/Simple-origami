cf32529ce54e85ecaef2b6430e791b80c225ac02
iago2345-hi <iago.pereira23@etepd.com>
2025-05-18 20:52:59 -0300

    tema.js

---
+++ RyanApp/src/constants/tema.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  red: '#FF4848',
+  darkRed: '#B53333',
+  topaz: '#903F3F',
+  background: '#FFE1E1',
+  white: '#FFFFFF',
+};
+
+export const FONTS = {
+  regular: 'System',
+  bold: 'System',
+};
